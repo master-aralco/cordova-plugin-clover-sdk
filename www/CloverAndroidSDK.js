e3cfc3898c1232bdbfa40eb979505eedecc64105
LinuxPony <sonic.ert@gmail.com>
2023-06-23 10:22:33 -0700

    Update CloverAndroidSDK.js

--- www/CloverAndroidSDK.js
+++ www/CloverAndroidSDK.js
@@ -8,6 +8,10 @@ exports.startScan = function(success, error) {
     exec(success, error, "CloverAndroidSDK", "startScan", []);
 };
 
+exports.printTextReceipt = function(receipt, success, error) {
+    exec(success, error, "CloverAndroidSDK", "printTextReceipt", [receipt]);
+};
+
 exports.processBarcode = function(barcodeResult) {
     return barcodeResult;
 };
